7790093de710fb73382699e6d568c25d2275c7f3
Vazgen Aleksanyan <vazgenaleksanyan9092@gmail.com>
2019-02-23 19:12:17 +0400

    content chenge

--- resume/build/js/main.js
+++ resume/build/js/main.js
@@ -1,7 +1,5 @@
 window.onload = function(){
   
-  let skills_level = document.querySelectorAll('.skills__level');
-  
   //  no supported ie 11
   
 //  skills_level.forEach(function(v){
@@ -12,19 +10,23 @@ window.onload = function(){
 //    }
 //  });
   
-  for(let j = 0; j<skills_level.length; j++){
-    let level_num = skills_level[j].attributes['data-skills-level'].value;
-    let level = skills_level[j].querySelectorAll('span');
-    for(let i = 0; i<level_num; i++){
-      level[i].classList.add('active');
+  let skills_level = document.querySelectorAll('.skills__level');
+  if(skills_level.length){
+    for(let j = 0; j<skills_level.length; j++){
+      let level_num = skills_level[j].attributes['data-skills-level'].value;
+      let level = skills_level[j].querySelectorAll('span');
+      for(let i = 0; i<level_num; i++){
+        level[i].classList.add('active');
+      }
     }
   }
   
   let soft_lavel = document.querySelectorAll('.soft__level');
-  for(let j = 0; j<soft_lavel.length; j++){
-    let level_num = soft_lavel[j].attributes['data-soft-level'].value;
-    let level = soft_lavel[j].querySelector('.soft__level_part');
-    level.style.width = level_num + '%';
+  if(soft_lavel.length){
+    for(let j = 0; j<soft_lavel.length; j++){
+      let level_num = soft_lavel[j].attributes['data-soft-level'].value;
+      soft_lavel[j].querySelector('.soft__level_part').style.width = level_num + '%';
+    }
   }
   
   
