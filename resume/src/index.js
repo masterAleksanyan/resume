fee756dfaf69e633b924349158c04b6a98135780
Vazgen Aleksanyan <vazgenaleksanyan9092@gmail.com>
2019-02-27 23:26:14 +0400

    added pug components

---
+++ resume/src/index.js
@@ -0,0 +1,7 @@
+//import Jquery from 'jquery'
+import './css/main.css'
+import './scss/main.scss'
+import './js/common'
+
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
